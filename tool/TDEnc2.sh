bf18305bc231d4f702fa7be2cf0e5701df068b84
tdenc <tdenc1125@gmail.com>
2017-07-07 12:32:51 +0900

    2.30

--- tool/TDEnc2.sh
+++ tool/TDEnc2.sh
@@ -7,7 +7,7 @@ cd "${current_dir}"
 
 ### Variables ### {{{
 # version of this script and x264
-current_version="2.29"
+current_version="2.30"
 current_x264_version=2358
 
 # make a directory for temporary files
